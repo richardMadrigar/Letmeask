cdf838bfc0daa2548a091ad8174375081061671b
Richard madrigar <richard_madrigar99@hotmail.com>
2021-06-24 00:59:45 -0300

    First

--- src/componets/RoomCode.tsx
+++ src/componets/RoomCode.tsx
@@ -19,7 +19,7 @@ export function RoomCode(props: RoomCodeProps) {
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #{props.code}</span>
+      <span>Salaa #{props.code}</span>
     </button>
   )
 }
