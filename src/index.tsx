c89c47b733008d8cfa941de8ce840b1cb11aadc3
Richard madrigar <richard_madrigar99@hotmail.com>
2021-06-24 00:57:19 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './services/firebase'
+
+import App from './App';
+
+import './styles/global.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/services/firebase.ts
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth'
+import 'firebase/database'
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_EMSSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: "G-21P2PF1M3N"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ const auth = firebase.auth();
+ const database = firebase.database()
+
+ export { firebase, auth, database}
